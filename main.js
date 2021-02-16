09c37efec03b4e1a7b9599a1221edb0c409b9e48
jeanPcr <jean.coding971@gmail.com>
2021-02-16 16:02:16 +0100

    Init recat app for electron

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+if (isDev) {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+  });
+}
+
+app.whenReady().then(createWindow);
